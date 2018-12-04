0b836c337e7f7d7f13d95ae550e418e383f29613
bsujatmiko <bagusiibag85@gmail.com>
2018-12-04 20:27:42 +0700

    landing page perdana website komunitas DELMANIKA

---
+++ js/init.js
@@ -0,0 +1,29 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+$(document).ready(function() {
+  $('.carousel.carousel-slider.today').carousel({
+    fullWidth: true
+  });
+
+  slideCarouselToday();
+  function slideCarouselToday() {
+      k = setInterval(function(){
+        $('.carousel.carousel-slider.today').carousel('next');
+      },6000);
+
+      $('.carousel.carousel-slider.today').mouseenter(function(event) {
+        clearInterval(k);
+      });
+      $('.carousel.carousel-slider.today').mouseleave(function(event) {
+        slideCarouselToday();
+      });
+  }
+});
